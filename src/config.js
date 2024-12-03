e51047ea45372b2eca11c6ab4707bce821fed605
Marco <mark_o002@hotmail.com>
2024-12-03 10:36:59 -0600

    path updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
+const path = require("path");
+
 // const PRODUCTS_URL = "https://www.promodescuentos.com/search?q=game%20pass";
 const PRODUCTS_HEALTH_URL = "https://www.promodescuentos.com";
 const PRODUCTS_URL = "https://www.promodescuentos.com/search?q=game%20pass";
-const FILENAME = "productos.txt";
+const FILENAME = path.join(__dirname, "productos.txt");
 
 const PHONE = {
   ADMIN: process.env.ADMIN_PHONE,
